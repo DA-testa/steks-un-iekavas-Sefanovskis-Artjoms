ff1ef484008079b5d919d3ead9db58bcba3e1af7
Sefanovskis-Artjoms <artjoms200@gmail.com>
2023-02-19 16:07:04 +0200

    pabeigts darbs

--- main.py
+++ main.py
@@ -10,22 +10,44 @@ def are_matching(left, right):
 
 
 def find_mismatch(text):
-    opening_brackets_stack = []
+    brackets_stack = []
+    counters = {'(': 0, ')': 0, '[': 0, ']': 0,'{': 0, '}': 0}
     for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+        if next in "([{)]}":
+            bracket = Bracket(next,i)
+            brackets_stack.append(bracket)
+            counters[next] = counters[next] + 1
+    
+    roundBrack = counters["("] - counters[")"]
+    squareBrack = counters["["] - counters["]"]
+    curlyBrack = counters["{"] - counters["}"]
 
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+    if roundBrack == 0 and squareBrack == 0 and curlyBrack == 0:
+        return False
+
+    elif roundBrack < 0 or squareBrack < 0 or curlyBrack < 0:
 
 
+        for i in brackets_stack[::-1]:
+            brackCounter = roundBrack if i.char == ")" else squareBrack if i.char == "]" else curlyBrack
+
+            if(i.char in ")]}" and brackCounter < 0):
+                return i.position
+    else:
+        for i in brackets_stack:
+            brackCounter = roundBrack if i.char == "(" else squareBrack if i.char == "[" else curlyBrack
+
+            if(i.char in "([{" and brackCounter > 0):
+                return i.position
+        
 def main():
-    text = input()
+    # text = input()
+    text = "(){}[]]}"
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-
+    if(mismatch == False):
+        print("Success")
+    else:
+        print(mismatch)
 
 if __name__ == "__main__":
     main()
